325c7d708b0449d2989b945bb14754cbaab78b7f
JoyceLA <joycseleneanaid@gmail.com>
2014-12-01 20:47:43 -0600

    Update bienvenido.php

--- app/bienvenido.php
+++ app/bienvenido.php
@@ -1,88 +1,79 @@
-<?php 
+<?php
 require_once "Usuario.php";
-session_start(); 
+session_start();
 
 require_once "functions.php";
 
+    $usuario = $_SESSION['user_id'];
+    $pass = $_SESSION['pass'];
 
+    /*$user0 = new Usuario("Maria23","452232Ga");
+    $user1= new Usuario("Maria23333","6371222qwss");
+    $user2 = new Usuario("Maria2782313","62hwsqwss");
+    $user3= new Usuario("Juana62132","6323");
 
-	$usuario = $_SESSION['user_id'];
-	$pass = $_SESSION['pass'];
- 
-	if(isset($_SESSION['usuario'])){
-       $user = $_SESSION['usuario'];
-
+    $user = array($user0,$user1, $user2, $user3);*/
+    if (isset($_SESSION['usuario'])) {
+        $user = $_SESSION['usuario'];
     }
-	
-	
+
+    if (isset($_POST['modificar'])) {
+        if ($_POST['password'] == $_POST['confpassword']) {
+            $userNow = new Usuario($usuario, $pass);
+
+            $userNew = new Usuario($_POST['user'], $_POST['password']);
+
+            $funcion = new Funciones();
+            $_SESSION['usuario'] = $funcion->updateProfile($user, $userNow, $userNew);
+
+            echo "<div align='center'>".$funcion->getMessageUpdate()."</div>";
+        }
+    }
+
+   if (isset($_POST['eliminar'])) {
+       if ($_POST['password'] == $_POST['confpassword']) {
+           $userNow = new Usuario($usuario, $pass);
+           $funcion = new Funciones();
+           $_SESSION['usuario'] = $funcion->deleteUser($user, $userNow);
+
+           echo "<div align='center'>".$funcion->getMessageDelete()."</div>";
+           header("location:index.php");
+       }
+   }
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml">
 		<head>
-			<meta http-equiv="Content-Type" content="text/html;charset=utf-8" /> 
+			<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 			<link rel="stylesheet" href="style/style.css" type="text/css" />
-			<title>Bienvenido</title> 
+			<title>Bienvenido</title>
 			<script language="JavaScript">
-				function mostrar(){ 
+				function mostrar(){
 					//document.write(x);
 				    //return;
-				    alert("Nuevo"); 
+				    alert("Nuevo");
 			    }
-			</script> 
+			</script>
 		</head>
 		<body>
-			
+
 		<center>
-			<form action="" method="post" class="login"> 
+			<form action="" method="post" class="login">
 				<div><label>Cuenta:</label><input name="user" type="text" value="<?php echo  $usuario ?>"></div>
 				<div><label>Contraseña:</label><input name="password" type="password" value="<?php echo $pass ?>"></div>
-				<div><label>Confirmar Contraseña:</label><input name="confpassword" type="password" value="<?php echo $pass ?>"></div>
+				<div>
+					<label>Confirmar Contraseña:</label>
+					<input name="confpassword" type="password" value="<?php echo $pass ?>">
+				</div>
 				<div>
 					<input name="modificar" type="submit" value="Modificar">
 					<input name="eliminar" type="submit" value="Eliminar";>
-				</div> 
-				
-			</form> 
-			
+				</div>
+
+			</form>
+
         </center>
 		</body>
 	</html>
-<?php
-
-    if(isset($_POST['modificar'])) {
-
-    	if($_POST['password'] == $_POST['confpassword']){
-    		$userNow = new Usuario($usuario,$pass);
-	       
-	        $userNew = new Usuario($_POST['user'],$_POST['password']);
-	        
-	        
-	        $funcion = new Funciones();
-	        $_SESSION['usuario'] = $funcion->updateProfile( $user,$userNow,$userNew); 
-	          
-	       echo "<div align='center'>".$funcion->getMessageUpdate()."</div>";
-	      
-	    }
-	           
-    }  
-
-
-   if(isset($_POST['eliminar'])) {
-
-    	if($_POST['password'] == $_POST['confpassword']){
-	        $userNow = new Usuario($usuario,$pass);
-	        $funcion = new Funciones();
-	        $_SESSION['usuario'] = $funcion->deleteUser($user,$userNow);
-	       
-	        echo "<div align='center'>".$funcion->getMessageDelete()."</div>"; 
-	        header("location:index.php"); 
-	    }
-	           
-    }  
-
-
-
-?>
-
