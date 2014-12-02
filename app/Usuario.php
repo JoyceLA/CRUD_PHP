d8a24cd145779c102c724dc855781448d36e1a60
JoyceLA <joycseleneanaid@gmail.com>
2014-12-01 20:47:21 -0600

    Update Usuario.php

--- app/Usuario.php
+++ app/Usuario.php
@@ -1,31 +1,54 @@
-<?php 
+<?php
+/**
+ * Usuario
+ *
+ * PHP version 5.5.9
+ *
+ * @category Class
+ * @package  App
+ * @author   <lumajo@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://www.hashbangcode.com/
+ *
+ */
+/**
+ * Usuario
+ *
+ * @category Class
+ * @package  App
+ * @author   <lumajo@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://www.hashbangcode.com/
+ *
+ */
+class Usuario
+{
+    private $_user;
+    private $_password;
 
-class Usuario{
-	private $user;
-	private $password;
-
-    function Usuario($us,$pass){
+    public function Usuario($us, $pass)
+    {
         $this->user = $us;
         $this->password = $pass;
     }
 
-    public function setUser($Usuario){
-    	$this->user = $Usuario;
+    public function setUser($usuario)
+    {
+        $this->user = $usuario;
     }
 
-    public function getUser(){
-    	return $this->user;
+    public function getUser()
+    {
+        return $this->user;
     }
 
-    public function setPassword($pass){
-    	$this->password = $pass;
+    public function setPassword($pass)
+    {
+        $this->password = $pass;
     }
 
-    public function getPassword(){
-    	return $this->password;
+    public function getPassword()
+    {
+        return $this->password;
     }
-
 }
-
-
-?>
